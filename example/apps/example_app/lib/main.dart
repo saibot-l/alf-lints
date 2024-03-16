9aed7ff387e725beec0d89548e1381245578ce20
Tobi <132131235+saibot-l@users.noreply.github.com>
2024-03-16 15:33:09 +0100

    docs: Extend examples
    Adds more imports. Adds package library.

--- example/apps/example_app/lib/main.dart
+++ example/apps/example_app/lib/main.dart
@@ -1,7 +1,23 @@
+import 'package:example_feature/example_feature.dart';
+import 'package:example_library/example_library.dart';
+
 import 'src/app/routes.dart';
+import 'src/features/a/feature_a.dart';
+import 'src/libraries/a/library_a.dart';
 
 class Main {
   final Routes routes;
+  final FeatureA featureA;
+  final LibraryA libraryA;
 
-  const Main(this.routes);
+  final ExampleFeature exampleFeature;
+  final ExampleLibrary exampleLibrary;
+
+  const Main(
+    this.routes,
+    this.featureA,
+    this.libraryA,
+    this.exampleFeature,
+    this.exampleLibrary,
+  );
 }
--- example/apps/example_app/lib/src/app/app.dart
+++ example/apps/example_app/lib/src/app/app.dart
@@ -1,8 +1,27 @@
+import 'package:example_feature/example_feature.dart';
+import 'package:example_library/example_library.dart';
+
 // expect_lint: avoid_import_entry_point
 import '../../main.dart';
+import '../features/a/feature_a.dart';
+import '../libraries/a/library_a.dart';
+import 'routes.dart';
 
 class App {
   final Main main;
+  final Routes routes;
+  final FeatureA featureA;
+  final LibraryA libraryA;
 
-  const App(this.main);
+  final ExampleFeature exampleFeature;
+  final ExampleLibrary exampleLibrary;
+
+  const App(
+    this.main,
+    this.routes,
+    this.featureA,
+    this.libraryA,
+    this.exampleFeature,
+    this.exampleLibrary,
+  );
 }
