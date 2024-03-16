29d0711eb3c284eb61f0096fa857f5eeaf2def7e
Tobi <132131235+saibot-l@users.noreply.github.com>
2024-03-16 09:35:10 +0100

    feat: Add `avoid_import_feature_from_library` lint
    Ensures that libraries don't import features.

--- example/apps/example_app/lib/src/libraries/a/library_a.dart
+++ example/apps/example_app/lib/src/libraries/a/library_a.dart
@@ -2,10 +2,13 @@
 import '../../../main.dart';
 // expect_lint: avoid_import_app_from_library
 import '../../app/pages/page_a.dart';
+// expect_lint: avoid_import_feature_from_library
+import '../../features/a/feature_a.dart';
 
 class LibraryA {
   final Main main;
   final PageA pageA;
+  final FeatureA featureA;
 
-  const LibraryA(this.main, this.pageA);
+  const LibraryA(this.main, this.pageA, this.featureA);
 }
--- lib/src/libraries/file_path/get_domain_groups.dart
+++ lib/src/libraries/file_path/get_domain_groups.dart
@@ -4,5 +4,9 @@ import 'is_path_of_domain.dart';
 List<String> getDomainGroups(String? relativePath, FileDomain domain) {
   if (relativePath == null) return [];
   if (!isPathOfDomain(relativePath, domain)) return [];
-  return relativePath.split(domain.path).last.split('/');
+  return relativePath
+      .replaceAll(RegExp(r'\.dart$'), '')
+      .split(domain.path)
+      .last
+      .split('/');
 }
